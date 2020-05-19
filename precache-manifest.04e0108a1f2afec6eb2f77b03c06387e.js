18c0ca823ff0462c2866a9736883fdf4e289c00c
Sam <samu.garcia.c@gmail.com>
2020-05-18 23:04:47 -0400

    Updates

---
+++ precache-manifest.04e0108a1f2afec6eb2f77b03c06387e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d49d2c1ac84d0ba85089a9c02d3acd",
+    "url": "/project-name/index.html"
+  },
+  {
+    "revision": "530a1abaf89e31db209b",
+    "url": "/project-name/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e065c901307938ddc3d3",
+    "url": "/project-name/static/css/main.f800a464.chunk.css"
+  },
+  {
+    "revision": "530a1abaf89e31db209b",
+    "url": "/project-name/static/js/2.be8a070c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-name/static/js/2.be8a070c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e065c901307938ddc3d3",
+    "url": "/project-name/static/js/main.4f32cc71.chunk.js"
+  },
+  {
+    "revision": "f0c1d46496cd76a9f438",
+    "url": "/project-name/static/js/runtime-main.0dd21160.js"
+  }
+]);
